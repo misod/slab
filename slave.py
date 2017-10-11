adbb1a0e81deb19a46bc38d4c51f2568dd9c198f
Mikael söderberg <mikael.misod@gmail.com>
2017-10-11 23:44:48 +0200

    test

--- slave.py
+++ slave.py
@@ -3,7 +3,7 @@ import psutil, time
 
 
 url = 'http://130.239.81.63:5000/slave/34/'
-for i in rage(10):
+for i in range(10):
     cpu = psutil.cpu_percent(interval=1, percpu=True)
     cpu_in_string = str(cpu[0])
     r = requests.get(url+cpu_in_string)

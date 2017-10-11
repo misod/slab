cbd2d7ae2fd9106a03092b929e7ffc7f96c94ce0
Mikael söderberg <mikael.misod@gmail.com>
2017-10-11 23:36:10 +0200

    test

--- slave.py
+++ slave.py
@@ -4,7 +4,8 @@ import psutil
 
 url = 'http://130.239.81.63:5000/slave/34/'
 cpu = psutil.cpu_percent(interval=1, percpu=True)
-r = requests.get(url+cpu[0])
+cpu_in_string = str(cpu[0])
+r = requests.get(url+cpu_in_string)
 response_json = r.json()
 
 print response_json

ff80225b4d59923b0c57e8352f7b4e4d02696869
Mikael söderberg <mikael.misod@gmail.com>
2017-10-11 22:49:18 +0200

    test

--- master.py
+++ master.py
@@ -5,5 +5,11 @@ app = Flask("master")
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/post/<int:slave_id>/int:cpu>')
+def get_info(slave_id, cpu):
+
+
+    return "info recived"
+
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')

abbdf55fcf690ca029c7e9f5f0fb8b12d5a9d595
Mikael söderberg <mikael.misod@gmail.com>
2017-10-11 23:44:08 +0200

    test

--- slave.py
+++ slave.py
@@ -1,11 +1,12 @@
 import requests, flask
-import psutil
+import psutil, time
 
 
 url = 'http://130.239.81.63:5000/slave/34/'
-cpu = psutil.cpu_percent(interval=1, percpu=True)
-cpu_in_string = str(cpu[0])
-r = requests.get(url+cpu_in_string)
-response_json = r.json()
-
-print response_json
+for i in rage(10):
+    cpu = psutil.cpu_percent(interval=1, percpu=True)
+    cpu_in_string = str(cpu[0])
+    r = requests.get(url+cpu_in_string)
+    response_json = r.json()
+    print response_json
+    time.sleep(2)

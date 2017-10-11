15eb4620089698ee8f108194fdcef1239d0a9830
Mikael söderberg <mikael.misod@gmail.com>
2017-10-11 23:10:08 +0200

    test

--- master.py
+++ master.py
@@ -5,17 +5,18 @@ app = Flask("master")
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/post/<int:slave_id>/<cpu>')
-def get_info(slave_id, cpu):
+@app.route('/slave/<int:slave_id>')
+def get_info(slave_id):
+    print slave_id
+
+    return "info recived \n"
+
+@app.route('/slave/<int:slave_id>/<int:cpu>')
+def get_info_cpu(slave_id, cpu):
     print slave_id
     print cpu
 
-# @app.route('/post/<int>/<int:slave_id>')
-# def get_info(slave_id):
-#     print slave_id
-#     #print cpu
-
-    return "info recived \n"
+    return "yheee"
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True)
